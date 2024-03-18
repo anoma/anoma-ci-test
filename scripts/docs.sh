3cd8d9ac2b74b3a9792bac693a02bde5f27b311f
mariari <mariari@protonmail.com>
2024-03-18 22:22:39 +0800

    try this out

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,5 +1,5 @@
+mix docs
 echo "var versionNodes = [" > ./doc/.doc-versions.js
 app=`mix run -e 'IO.puts(Mix.Project.config()[:app])'`; \
 for v in $(git tag | tac); do echo "{version: \"$v\", url: \"https://anoma.github.io/anoma/$v/\"}," >> ./doc/.doc-versions.js; done
 echo "]" >> ./doc/.doc-versions.js
-mix docs
