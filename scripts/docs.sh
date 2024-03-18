2237f161c0fea68145510bfdadb18b43e095fe23
mariari <mariari@protonmail.com>
2024-03-18 20:16:22 +0800

    Add docs generation with versioning
    This works by using a shell script to generate out a .doc-versions.js.
    
    This file should look something like
    
    ```js
    var versionNodes = [
    {version: "v0.9.0", url: "https://anoma.github.io/anoma/v0.9.0/"},
    {version: "v0.8.0", url: "https://anoma.github.io/anoma/v0.8.0/"},
    {version: "v0.7.0", url: "https://anoma.github.io/anoma/v0.7.0/"},
    {version: "v0.6.0", url: "https://anoma.github.io/anoma/v0.6.0/"},
    {version: "v0.5.0", url: "https://anoma.github.io/anoma/v0.5.0/"},
    {version: "v0.4.0", url: "https://anoma.github.io/anoma/v0.4.0/"},
    {version: "v0.3.0", url: "https://anoma.github.io/anoma/v0.3.0/"},
    {version: "v0.2.0", url: "https://anoma.github.io/anoma/v0.2.0/"},
    {version: "v0.10.0", url: "https://anoma.github.io/anoma/v0.10.0/"},
    {version: "v0.1.0", url: "https://anoma.github.io/anoma/v0.1.0/"},
    ]
    ```
    
    This should be further filtered and organized.
    
    Further, this should be merged with the corrsponding versions for each
    docs.
    
    squash

---
+++ scripts/docs.sh
@@ -0,0 +1,5 @@
+echo "var versionNodes = [" > ./doc/.doc-versions.js
+app=`mix run -e 'IO.puts(Mix.Project.config()[:app])'`; \
+for v in $(git tag | tac); do echo "{version: \"$v\", url: \"https://anoma.github.io/anoma/$v/\"}," >> ./doc/.doc-versions.js; done
+echo "]" >> ./doc/.doc-versions.js
+mix docs
